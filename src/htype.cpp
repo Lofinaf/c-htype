d316db78be8691b3972404b97328600862bb5aae
Lofinaf <86482845+Lofinaf@users.noreply.github.com>
2022-06-01 13:52:01 +0300

    Add files via upload

---
+++ src/htype.cpp
@@ -0,0 +1,41 @@
+﻿#include <string>
+#include <iostream>
+
+
+using namespace std;
+
+class h
+{
+
+	public:
+		let(int i)
+		{
+			value = i;
+		}
+		void print()
+		{
+			if (value == NULL)
+			{
+				cout << "Error! Error code 01" << endl;
+			}
+			else
+			{
+				cout << value << endl;
+			}
+			
+		}
+		void del()
+		{
+			delete this;
+		}
+	private:
+		int value;
+
+};
+
+int main()
+{
+	h a = 0;
+
+	return 0;
+}
